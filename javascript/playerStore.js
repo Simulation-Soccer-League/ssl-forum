9cd84c574ddb7cc91863d9bc24b6be2618c273e7
Canadice <33020567+canadice@users.noreply.github.com>
2023-03-15 12:39:14 +0100

    Testing fetching player and populating

---
+++ javascript/playerStore.js
@@ -0,0 +1,17 @@
+// Fetches the build of the player whose user is logged in
+async function fetchPlayer() {
+    const url = "https://api.simulationsoccer.com/ssl/getPlayer?username=" + username
+
+    const playerData = fetch(url)
+
+    playerData
+        .then((response) => response.json())
+        .then((data) => {
+            // data contains in its [0] index, the javascript object with all information
+            document.querySelector("#playerName").innerText = data[0].Name
+
+            document.querySelector("#playerBank").innerText = data[0].Name
+
+        });
+
+}
