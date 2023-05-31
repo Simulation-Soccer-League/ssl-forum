42d2d867297a6ac92e6ee590e552ea4741e45548
Canadice <33020567+canadice@users.noreply.github.com>
2023-05-31 20:10:20 +0200

    Updated checklist

--- javascript/taskChecklist.js
+++ javascript/taskChecklist.js
@@ -1,15 +1,58 @@
-async function fetchPlayer() {
-    const url = "https://api.simulationsoccer.com/ssl/currentPTs?username=" + username
+async function fetchChecklist() {
+    const url = "https://api.simulationsoccer.com/ssl/currentPTs?user=" + username
 
     const playerData = fetch(url)
 
     playerData
         .then((response) => response.json())
         .then((data) => {
-            // data contains in its [0] index, the javascript object with all information
-            document.querySelector("#playerName").innerText = data[0].Name
+            let cappedPT = data.slice(0, 5);
+
+            let uncappedPT = data.slice(5, data.length);
 
 
+            var capDiv = document.querySelector("#cappedPT");
+            var capUl = document.createElement("ul");
+
+            cappedPT.forEach(element => {
+                var li = document.createElement("li");
+                var link = document.createElement("a");
+
+                link.href = element.Link
+                link.appendChild(document.createTextNode(element.Thread))
+
+                li.appendChild(link)
+
+                if (!(element.User === null)) {
+                    li.classList.add("strikethrough");
+                }
+
+                capUl.appendChild(li)
+            });
+
+            capDiv.appendChild(capUl)
+
+            var uncapDiv = document.querySelector("#uncappedPT");
+            var uncapUl = document.createElement("ul");
+
+            uncappedPT.forEach(element => {
+                var li = document.createElement("li");
+                var link = document.createElement("a");
+
+                link.href = element.Link
+                link.appendChild(document.createTextNode(element.Thread))
+
+                li.appendChild(
+                    link
+                )
+                uncapUl.appendChild(li)
+            });
+
+            uncapDiv.appendChild(uncapUl)
+
+            console.log(data.length + 1)
+            console.log(cappedPT)
+            console.log(uncappedPT)
 
         });
 }
